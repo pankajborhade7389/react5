c1ae512711eefc41433d00902a70dfaa96e78741
unknown <pankajborhade333@gmail.com>
2022-05-27 14:41:25 +0530

    react-fifth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="components">
+
+      <h1>Higher Order Component :-</h1>
+          <ul type="disc">
+              <li>
+              A higher-order component (HOC) is an advanced technique in React for reusing component logic.
+              </li>
+              <br></br>
+              <li>
+              HOC is a function that takes a component and returns a new component.
+              </li>
+          </ul>
+
+          <h1>Pure Component :-</h1>
+          <ul type="disc">
+              <li>
+              A React component is considered pure if it renders the same output for the same state and props
+              </li>
+              <br></br>
+              <li>
+              Component Class compares current state and props with new props and states to decide whether the React component should re-render itself or Not.
+              </li>
+              <br></br>
+              <li>
+              If the previous value of state or props and the new value of state or props is same then component will not re-render.
+              </li>
+          </ul>   
+             
+      </div>
   );
 }
 
